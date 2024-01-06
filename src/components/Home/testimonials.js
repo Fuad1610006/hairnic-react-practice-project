c0257eedd5f5d907155cc8978bdcde4650582435
Fuad1610006 <afmfuad96@gmail.com>
2024-01-05 16:13:20 -0800

    Testimonial added

--- src/components/Home/testimonials.js
+++ src/components/Home/testimonials.js
@@ -1,66 +1,51 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom if you're using it
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-const TestimonialItem = ({ imgSrc, content, clientName, profession }) => {
+   
+const Testimonial = () => {
+   const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    };
   return (
-    <div className="testimonial-item text-center">
-      <img className="img-fluid border p-2" src={`assets/img/${imgSrc}`} alt="" />
-      <h5 className="fw-light lh-base text-white">{content}</h5>
-      <h5 className="mb-1">{clientName}</h5>
-      <h6 className="fw-light text-white fst-italic mb-0">{profession}</h6>
-    </div>
-  );
-};
-
-const TestimonialSection = () => {
-  const testimonials = [
-    {
-      imgSrc: 'testimonial-1.jpg',
-      content:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet, erat non malesuada consequat, nibh erat tempus risus...',
-      clientName: 'Client Name',
-      profession: 'Profession',
-    },
-    {
-      imgSrc: 'testimonial-2.jpg',
-      content:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet, erat non malesuada consequat, nibh erat tempus risus...',
-      clientName: 'Client Name',
-      profession: 'Profession',
-    },
-    {
-      imgSrc: 'testimonial-3.jpg',
-      content:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet, erat non malesuada consequat, nibh erat tempus risus...',
-      clientName: 'Client Name',
-      profession: 'Profession',
-    },
-  ];
-
-  return (
-    <div className="container-fluid testimonial bg-primary my-5 py-5">
-      <div className="container text-white py-5">
-        <div className="mx-auto text-center wow fadeIn" data-wow-delay="0.1s" style={{ maxWidth: '600px' }}>
-          <h1 className="text-white mb-3">
-            Our Customer Said <span className="fw-light text-dark">About Our Natural Shampoo</span>
-          </h1>
-          <p className="mb-5">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet, erat non malesuada consequat, nibh
-            erat tempus risus.
-          </p>
-        </div>
-        <div className="row justify-content-center">
-          <div className="col-lg-8">
-            <div className="owl-carousel testimonial-carousel wow fadeIn" data-wow-delay="0.1s">
-              {testimonials.map((testimonial, index) => (
-                <TestimonialItem key={index} {...testimonial} />
-              ))}
+     <div className="container-fluid testimonial bg-primary my-5 py-5">
+        <div className="container text-white py-5">
+            <div className="mx-auto text-center wow fadeIn" data-wow-delay="0.1s" style={{ maxWidth: '600px' }}>
+            <h1 className="text-white mb-3">Our Customer Said <span className="fw-light text-dark">About Our Natural Shampoo</span></h1>
+            <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet, erat non malesuada consequat, nibh erat tempus risus.</p>
             </div>
-          </div>
+            <div className="row justify-content-center">
+            <div className="col-lg-8">
+                <Slider {...settings}>
+                <div className="testimonial-item text-center">
+                    <img className="img-fluid border p-2" src="assets/img/testimonial-1.jpg" alt="" />
+                    <h5 className="fw-light lh-base text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet, erat non malesuada consequat, nibh erat tempus risus, vitae porttitor purus nisl vitae purus. Praesent tristique odio ut rutrum pellentesque. Fusce eget molestie est, at rutrum est. Nullam scelerisque libero nunc, vitae ullamcorper elit volutpat ut.</h5>
+                    <h5 className="mb-1">Client Name</h5>
+                    <h6 className="fw-light text-white fst-italic mb-0">Profession</h6>
+                </div>
+                <div className="testimonial-item text-center">
+                    <img className="img-fluid border p-2" src="assets/img/testimonial-2.jpg" alt="" />
+                    <h5 className="fw-light lh-base text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet, erat non malesuada consequat, nibh erat tempus risus, vitae porttitor purus nisl vitae purus. Praesent tristique odio ut rutrum pellentesque. Fusce eget molestie est, at rutrum est. Nullam scelerisque libero nunc, vitae ullamcorper elit volutpat ut.</h5>
+                    <h5 className="mb-1">Client Name</h5>
+                    <h6 className="fw-light text-white fst-italic mb-0">Profession</h6>
+                </div>
+                <div className="testimonial-item text-center">
+                    <img className="img-fluid border p-2" src="assets/img/testimonial-3.jpg" alt="" />
+                    <h5 className="fw-light lh-base text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet, erat non malesuada consequat, nibh erat tempus risus, vitae porttitor purus nisl vitae purus. Praesent tristique odio ut rutrum pellentesque. Fusce eget molestie est, at rutrum est. Nullam scelerisque libero nunc, vitae ullamcorper elit volutpat ut.</h5>
+                    <h5 className="mb-1">Client Name</h5>
+                    <h6 className="fw-light text-white fst-italic mb-0">Profession</h6>
+                </div>
+                </Slider>
+            </div>
+            </div>
         </div>
-      </div>
-    </div>
+     </div>
   );
 };
 
-export default TestimonialSection;
+export default Testimonial;
--- src/components/Testominial/testimonial.js
+++ src/components/Testominial/testimonial.js
@@ -2,8 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../Header/header';
 import Footer from '../Footer/footer';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const Testimonial = () => {
+     const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    };
+    
   return (
       <div>
           <Header/>
@@ -55,40 +66,43 @@ const Testimonial = () => {
       </div>
     </div>
           {/* Feature start*/}
+
+          {/* Testimonials start*/}
           
-<div className="container-fluid testimonial bg-primary my-5 py-5">
-      <div className="container text-white py-5">
-        <div className="mx-auto text-center wow fadeIn" data-wow-delay="0.1s" style={{ maxWidth: '600px' }}>
-          <h1 className="text-white mb-3">Our Customer Said <span className="fw-light text-dark">About Our Natural Shampoo</span></h1>
-          <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet, erat non malesuada consequat, nibh erat tempus risus.</p>
-        </div>
-        <div className="row justify-content-center">
-          <div className="col-lg-8">
-            <div className="owl-carousel testimonial-carousel wow fadeIn" data-wow-delay="0.1s">
-              <div className="testimonial-item text-center" data-dot="1">
-                <img className="img-fluid border p-2" src="assets/img/testimonial-1.jpg" alt="" />
-                <h5 className="fw-light lh-base text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet, erat non malesuada consequat, nibh erat tempus risus, vitae porttitor purus nisl vitae purus. Praesent tristique odio ut rutrum pellentesque. Fusce eget molestie est, at rutrum est. Nullam scelerisque libero nunc, vitae ullamcorper elit volutpat ut.</h5>
-                <h5 className="mb-1">Client Name</h5>
-                <h6 className="fw-light text-white fst-italic mb-0">Profession</h6>
-              </div>
-              <div className="testimonial-item text-center" data-dot="2">
-                <img className="img-fluid border p-2" src="assets/img/testimonial-2.jpg" alt="" />
-                <h5 className="fw-light lh-base text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet, erat non malesuada consequat, nibh erat tempus risus, vitae porttitor purus nisl vitae purus. Praesent tristique odio ut rutrum pellentesque. Fusce eget molestie est, at rutrum est. Nullam scelerisque libero nunc, vitae ullamcorper elit volutpat ut.</h5>
-                <h5 className="mb-1">Client Name</h5>
-                <h6 className="fw-light text-white fst-italic mb-0">Profession</h6>
-              </div>
-              <div className="testimonial-item text-center" data-dot="3">
-                <img className="img-fluid border p-2" src="assets/img/testimonial-3.jpg" alt="" />
-                <h5 className="fw-light lh-base text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet, erat non malesuada consequat, nibh erat tempus risus, vitae porttitor purus nisl vitae purus. Praesent tristique odio ut rutrum pellentesque. Fusce eget molestie est, at rutrum est. Nullam scelerisque libero nunc, vitae ullamcorper elit volutpat ut.</h5>
-                <h5 className="mb-1">Client Name</h5>
-                <h6 className="fw-light text-white fst-italic mb-0">Profession</h6>
-              </div>
+    <div className="container-fluid testimonial bg-primary my-5 py-5">
+        <div className="container text-white py-5">
+            <div className="mx-auto text-center wow fadeIn" data-wow-delay="0.1s" style={{ maxWidth: '600px' }}>
+            <h1 className="text-white mb-3">Our Customer Said <span className="fw-light text-dark">About Our Natural Shampoo</span></h1>
+            <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet, erat non malesuada consequat, nibh erat tempus risus.</p>
             </div>
-          </div>
+            <div className="row justify-content-center">
+            <div className="col-lg-8">
+                <Slider {...settings}>
+                <div className="testimonial-item text-center">
+                    <img className="img-fluid border p-2" src="assets/img/testimonial-1.jpg" alt="" />
+                    <h5 className="fw-light lh-base text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet, erat non malesuada consequat, nibh erat tempus risus, vitae porttitor purus nisl vitae purus. Praesent tristique odio ut rutrum pellentesque. Fusce eget molestie est, at rutrum est. Nullam scelerisque libero nunc, vitae ullamcorper elit volutpat ut.</h5>
+                    <h5 className="mb-1">Client Name</h5>
+                    <h6 className="fw-light text-white fst-italic mb-0">Profession</h6>
+                </div>
+                <div className="testimonial-item text-center">
+                    <img className="img-fluid border p-2" src="assets/img/testimonial-2.jpg" alt="" />
+                    <h5 className="fw-light lh-base text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet, erat non malesuada consequat, nibh erat tempus risus, vitae porttitor purus nisl vitae purus. Praesent tristique odio ut rutrum pellentesque. Fusce eget molestie est, at rutrum est. Nullam scelerisque libero nunc, vitae ullamcorper elit volutpat ut.</h5>
+                    <h5 className="mb-1">Client Name</h5>
+                    <h6 className="fw-light text-white fst-italic mb-0">Profession</h6>
+                </div>
+                <div className="testimonial-item text-center">
+                    <img className="img-fluid border p-2" src="assets/img/testimonial-3.jpg" alt="" />
+                    <h5 className="fw-light lh-base text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet, erat non malesuada consequat, nibh erat tempus risus, vitae porttitor purus nisl vitae purus. Praesent tristique odio ut rutrum pellentesque. Fusce eget molestie est, at rutrum est. Nullam scelerisque libero nunc, vitae ullamcorper elit volutpat ut.</h5>
+                    <h5 className="mb-1">Client Name</h5>
+                    <h6 className="fw-light text-white fst-italic mb-0">Profession</h6>
+                </div>
+                </Slider>
+            </div>
+            </div>
         </div>
-      </div>
-    </div>
-   
+     </div>
+            {/* Testimonials end*/}
+    
     <Footer />
     </div>
   );
